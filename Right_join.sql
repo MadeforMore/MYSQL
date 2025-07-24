94eee996d0590f481f5911da33f0da64c40ae3bf
Aryan Thite <167121724+MadeforMore@users.noreply.github.com>
2025-07-24 16:19:57 +0530

    Create Right_join.sql

---
+++ Right_join.sql
@@ -0,0 +1,47 @@
+-- Create student table
+CREATE TABLE student (
+    student_id INT PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+-- Create course table
+CREATE TABLE course (
+    student_id INT,
+    course VARCHAR(50)
+);
+
+-- Insert data into student table
+INSERT INTO student (student_id, name) VALUES
+(101, 'adam'),
+(102, 'bob'),
+(103, 'casey');
+
+-- Insert data into course table
+INSERT INTO course (student_id, course) VALUES
+(102, 'english'),
+(105, 'math'),
+(103, 'science'),
+(107, 'computer science');
+
+
+SELECT * FROM student;
+
+SELECT * FROM course;
+
+SELECT *
+FROM student
+RIGHT JOIN course
+ON student.student_id = course.student_id ;
+
+SELECT *
+FROM course
+RIGHT JOIN student
+ON student.student_id = course.student_id ;
+
+
+
+
+
+
+
+
