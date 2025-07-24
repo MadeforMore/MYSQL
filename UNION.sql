c3b5f314004a94c805635edd53fff138d07bec9e
Aryan Thite <167121724+MadeforMore@users.noreply.github.com>
2025-07-24 17:15:04 +0530

    Create UNION.sql

---
+++ UNION.sql
@@ -0,0 +1,33 @@
+-- Create student table
+CREATE TABLE student (
+    student_id INT PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+-- Create course table
+CREATE TABLE course (
+    student_id INT,
+    course VARCHAR(50)
+);
+
+-- Insert data into student table
+INSERT INTO student (student_id, name) VALUES
+(101, 'adam'),
+(102, 'bob'),
+(103, 'casey');
+
+-- Insert data into course table
+INSERT INTO course (student_id, course) VALUES
+(102, 'english'),
+(105, 'math'),
+(103, 'science'),
+(107, 'computer science');
+
+
+SELECT * FROM student;
+
+SELECT * FROM course;
+
+SELECT student_id FROM student 
+UNION 
+SELECT student_id FROM course;
