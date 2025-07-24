9b068b46448c98e76d6c64858e93b0f498c8177b
Aryan Thite <167121724+MadeforMore@users.noreply.github.com>
2025-07-24 16:40:59 +0530

    Create Left_and_Right_Exclusive_Join.sql

---
+++ Left_and_Right_Exclusive_Join.sql
@@ -0,0 +1,44 @@
+-- Create student table
+CREATE TABLE student (
+    student_id INT PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+-- Create course table
+CREATE TABLE course (
+    student_id INT,
+    course VARCHAR(50)
+);
+
+-- Insert data into student table
+INSERT INTO student (student_id, name) VALUES
+(101, 'adam'),
+(102, 'bob'),
+(103, 'casey');
+
+-- Insert data into course table
+INSERT INTO course (student_id, course) VALUES
+(102, 'english'),
+(105, 'math'),
+(103, 'science'),
+(107, 'computer science');
+
+
+SELECT * FROM student;
+
+SELECT * FROM course;
+
+SELECT *
+FROM student
+LEFT JOIN course
+ON student.student_id = course.student_id 
+WHERE course.student_id IS NULL ;
+
+
+SELECT *
+FROM student 
+RIGHT JOIN course
+ON student.student_id = course.student_id 
+WHERE student.student_id IS NULL ;
+
+
