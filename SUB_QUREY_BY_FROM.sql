90fa50172cd11fee3109000c1a840242c9bfae2f
Aryan Thite <167121724+MadeforMore@users.noreply.github.com>
2025-07-24 18:02:20 +0530

    Create SUB_QUREY_BY_FROM.sql

---
+++ SUB_QUREY_BY_FROM.sql
@@ -0,0 +1,19 @@
+-- Create student table
+CREATE TABLE student (
+    rollno INT PRIMARY KEY,
+    name VARCHAR(50),
+    marks INT,
+    city VARCHAR(50)
+);
+
+-- Insert data into student table
+INSERT INTO student (rollno, name, marks, city) VALUES
+(101, 'anil', 78, 'Pune'),
+(102, 'bhumika', 93, 'Mumbai'),
+(103, 'chetan', 85, 'Mumbai'),
+(104, 'dhruv', 96, 'Delhi'),
+(105, 'emanuel', 92, 'Delhi'),
+(106, 'farah', 82, 'Delhi');
+
+SELECT MAX(marks)
+FROM (SELECT * FROM student WHERE city = "Delhi" ) as a;
